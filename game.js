4c0d440949f725a63cb8fb51869f42dbcc507c3a
Liam <kingliam28@ntlworld.com>
2015-09-14 09:27:23 +0100

    Remove invisible wall and make things behave like squares.

--- game.js
+++ game.js
@@ -7,10 +7,10 @@ canvas.height = document.body.clientHeight;
 
 var board = {
     width: 60,
-    height: 60
+    height: 35
 };
 
-var width = Math.round(canvas.height / board.width);
+var width = Math.round(canvas.width / board.width);
 var height = Math.round(canvas.height / board.height);
 
 var ctx = canvas.getContext('2d');
@@ -31,7 +31,7 @@ var directions = {
 
 var direction = directions.right;
 
-window.onkeydown = function(e) {
+window.onkeydown = function (e) {
     var key = e.keyCode;
 
     switch (key) {
@@ -75,24 +75,36 @@ var food = {
 var points = 0;
 
 // game loop
-setInterval(function() {
+setInterval(function () {
     // clear the entire canvas on each frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     switch (direction) {
-        case directions.up: snake.position.y--; break;
-        case directions.left: snake.position.x--; break;
-        case directions.down: snake.position.y++; break;
-        case directions.right: snake.position.x++; break;
+        case directions.up:
+            snake.position.y--;
+            break;
+        case directions.left:
+            snake.position.x--;
+            break;
+        case directions.down:
+            snake.position.y++;
+            break;
+        case directions.right:
+            snake.position.x++;
+            break;
     }
 
     if (snake.position.x === food.position.x && snake.position.y === food.position.y) {
         points++;
 
         food.position = {
-            x: Math.round(Math.random() * board.width), 
+            x: Math.round(Math.random() * board.width),
             y: Math.round(Math.random() * board.height)
         };
+    }
+
+    if (collisionWithEdge()) {
+        restart();
     }
 
     ctx.beginPath();
@@ -103,15 +115,41 @@ setInterval(function() {
 }, frameTimeMilliseconds);
 
 function drawSnake() {
-    ctx.rect(snake.position.x * width, snake.position.y * height, height, width);
+    ctx.rect(snake.position.x * width, snake.position.y * width, height, height);
 }
 
 function drawFood() {
-    ctx.fillRect(food.position.x * width, food.position.y * height, height, width);
+    ctx.fillRect(food.position.x * width, food.position.y * width, height, height);
 }
 
 function drawPoints() {
-    ctx.font = '12px serif';
-    ctx.fillText('Points: ' + points, 10, 20);
+    ctx.font = '30px serif';
+    ctx.fillText('Points: ' + points, 10, 40);
 }
 
+function collisionWithEdge() {
+    if (snake.position.x > board.width) {
+        return true;
+    }
+    else if (snake.position.x < 0) {
+        return true;
+    }
+    else if (snake.position.y < 0) {
+        return true;
+    }
+    else if (snake.position.y > board.height) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function restart() {
+    snake.position.x = 10;
+    snake.position.y = 4;
+    food.position.x = 20;
+    food.position.y = 20;
+    points = 0;
+    direction = directions.right
+}
